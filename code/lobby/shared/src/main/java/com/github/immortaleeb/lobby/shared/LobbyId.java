7282f2290e57b2e23e56b14333dbb05d19406bba
immortaleeb <immortaleeb@gmail.com>
2021-07-24 15:43:45 +0200

    Properly implement LobbyId

--- code/lobby/shared/src/main/java/com/github/immortaleeb/lobby/shared/LobbyId.java
+++ code/lobby/shared/src/main/java/com/github/immortaleeb/lobby/shared/LobbyId.java
@@ -1,9 +1,34 @@
 package com.github.immortaleeb.lobby.shared;
 
+import java.util.UUID;
+
 public class LobbyId {
 
+    private final String lobbyId;
+
+    private LobbyId(String lobbyId) {
+        this.lobbyId = lobbyId;
+    }
+
+    public String asString() {
+        return lobbyId;
+    }
+
+    @Override
+    public String toString() {
+        return asString();
+    }
+
+    public static LobbyId of(String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("Invalid lobby id");
+        }
+
+        return new LobbyId(value);
+    }
+
     public static LobbyId generate() {
-        return new LobbyId();
+        return of(UUID.randomUUID().toString());
     }
 
 }
